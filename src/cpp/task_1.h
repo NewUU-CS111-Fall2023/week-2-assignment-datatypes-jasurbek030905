c3321d23d5ec907fd149939e633a8ad0abd7b92f
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2023-10-25 23:05:44 +0500

    Update task_1.h

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -3,3 +3,33 @@
  * Date:
  * Name:
  */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    if (n < 10) {
+        if (n % 4 != 0) cout << 13 << "/" << 0 << 9 << "/" << 0 << 0 << 0 << n << endl;
+        else if (n % 100 == 0 and n % 400 != 0) cout << 13 << "/" << 0 << 9 << "/" << 0 << 0 << 0 << n << endl;
+        else cout << 12 << "/" << 0 << 9 << "/" << 0 << 0 << 0 << n;
+    } else {
+        if (n < 100) {
+            if (n % 4 != 0) cout << 13 << "/" << 0 << 9 << "/" << 0 << 0 << n << endl;
+            else if (n % 100 == 0 and n % 400 != 0) cout << 13 << "/" << 0 << 9 << "/" << 0 << 0 << n << endl;
+            else cout << 12 << "/" << 0 << 9 << "/" << 0 << 0 << n;
+        } else if (n < 1000) {
+            if (n % 4 != 0) cout << 13 << "/" << 0 << 9 << "/" << 0 << n << endl;
+            else if (n % 100 == 0 and n % 400 != 0) cout << 13 << "/" << 0 << 9 << "/" << 0 << n << endl;
+            else cout << 12 << "/" << 0 << 9 << "/" << 0 << n;
+        } else {
+            if (n % 4 != 0) cout << 13 << "/" << 0 << 9 << "/" << n << endl;
+            else if (n % 100 == 0 and n % 400 != 0) cout << 13 << "/" << 0 << 9 << "/" << n << endl;
+            else cout << 12 << "/" << 0 << 9 << "/" << n;
+        }
+
+
+    }
+    return 0;
+}
