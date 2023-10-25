4640eb57763f21c5199f3bc34c7c384c4482cd86
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2023-10-25 23:24:26 +0500

    Update task_2.h

--- src/cpp/task_2.h
+++ src/cpp/task_2.h
@@ -1,1 +1,23 @@
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    srand(time(0)); 
+    int secretNumber = rand() % 100 + 1;
+    int myguess;
+    int attempts = 0;
+    do {
+       cout << "Enter your guess (between 1 and 100): ";
+       cin >> myguess;
+        attempts++;
+        if (myguess > secretNumber) {
+            cout << "Too high!" << endl;
+        } else if (myguess < secretNumber) {
+            cout << "Too low!" << endl;
+        } else {
+            cout << "Congratulations! You found the secret number in " << attempts << " attempts." << std::endl;
+        }
+    } while (myguess != secretNumber);
+    return 0;
+}
